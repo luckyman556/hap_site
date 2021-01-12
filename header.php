7598b53cb6313ab783d91e639172fa2c0bbc2410
luckyman556 <luckyman556@gmail.com>
2021-01-12 12:58:19 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,38 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Hap your bunny wrote</title>
+
+
+    <link href='css/reset.css' rel='stylesheet'/>
+    <link href='css/vars.css' rel='stylesheet'/>
+    <link href='css/style.css' rel='stylesheet'/>
+
+</head>
+<body>
+<header>
+    <div class="header">
+        <div class="row">
+            <div class="header__menu-btn">
+                <div class="lines">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="text">Menu</div>
+            </div>
+            <div class="header__logo">
+                <img src="img/hap-site-logo.svg" alt="">
+            </div>
+            <div class="header__btns-block">
+                <a href="">Call</a>
+                <a href="">Apply</a>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ parts/first-block.php
@@ -0,0 +1,6 @@
+<div class="top-block">
+    <img class="top-block__bg" src="img/first-block-bg.jpg" alt="">
+    <h1>rent the <br>      unexpected</h1>
+    <p>A BOUTIQUE OFFERING OF <br> STUDIO, ONE AND <br> TWO-BEDROOM RESIDENCES</p>
+    <div class="top-block__scroll-down"></div>
+</div>
