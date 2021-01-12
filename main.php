eb562c8893979b9a837c8c56a3173e1a762f0080
luckyman556 <luckyman556@gmail.com>
2021-01-12 14:25:11 +0200

    text with img 1/1

--- main.php
+++ main.php
@@ -1,4 +1,5 @@
 <?php
 require_once 'header.php';
 require_once  'parts/first-block.php';
+require_once  'parts/img-with-text-1_1.php';
 require_once  'footer.php';
---
+++ parts/img-with-text-1_1.php
@@ -0,0 +1,9 @@
+<div class="img-text-1_1">
+    <div class="img-part">
+        <img src="img/hap-img-1.jpg" alt="">
+    </div>
+    <div class="text-part">
+        <h2 class="common-title">STATE<br>  YOUR<br>INDEPENDENCE</h2>
+        <div class="text-block">Positioned at the nexus of Manhattan’s most inspirational neighborhoods, this boutique collection of rental apartments is designed for people who don’t want to follow the straight lines. A bold angular façade that you can’t help but stare. A membership residents’ club that allows you to play your way. Your new address is a great canvas to make your mark right where you want to be. This is where Chelsea is moving, right by the High Line and Hudson Yards.</div>
+    </div>
+</div>
